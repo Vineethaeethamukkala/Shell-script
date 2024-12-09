63fd1577891d431da2f773dc9e590054996b2340
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 21:34:21 +0530

    hellow-world2

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,4 +5,4 @@ echo " your username is $username"
 
 echo "please enter you password"
 read -s password
-echo -e "\e[31m your password is $password"
+echo -e "\e[31m your password is $password, \e[0m"
