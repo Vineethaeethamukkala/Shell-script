44de74497b6fec67f2b30da7632703810c69f6f0
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 23:22:16 +0530

    hellow-world2

--- 13-loops-installm.sh
+++ 13-loops-installm.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 scriptname=$0
 date=$(date "+%F %H:%M:%S")
-logfile=/tmp/$scriptname.$date
+logfile=/tmp/$scriptname.$date.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+validate(){
+    if ( $? -ne 0)
+    then
+        echo "$2 $R .. filure $N"
+    else
+        echo "$2 $G .. Success $N"
+}
 for i in $@
 do
     yum install $i -y &>>$logfile
+    exit 1
 done
+validate $? "$i installation"
