ac987922f6c7d598fdcda9c687575a52e756c727
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 22:10:10 +0530

    hellow-world2

---
+++ 09-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+userid=$(id -u)  #here it will check if it is root / normal user
+
+validate(){
+    if [ $? -ne 0 ]
+    then
+        echo "$2 is failure"
+        exit 1
+    else
+        echo "$2 is sucess"
+    fi
+
+}
+
+if [ $userid -ne 0 ]
+then
+    echo "error please swith to root user"
+    exit 1
+else
+    echo "your are in root user"
+fi
+
+yum install mysql -y
+
+validate $? "mysql installation"
+
+yum install nginx -y
+
+validate $? "nginx installation"
+
+
