6cc335c788f917d9f85b31e6ffad4d87a5e27d10
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 22:47:10 +0530

    hellow-world2

--- 11-functions-logs.sh
+++ 11-functions-logs.sh
@@ -7,17 +7,17 @@ userid=$(id -u)
 validate() {
     if [ $? -ne 0 ]
     then
-       echo -e "\e[31m $2 .. failure"
+       echo -e "\e[31m $2 .. failure \e[0m"
     else
-    echo -e "\e[32m $2 ... success"
+    echo -e "\e[32m $2 ... success \e[0m"
 
     fi
 }
 if [ $userid -ne 0 ]
 then
-    echo -e "\e[31m please switch to root user"
+    echo -e "\e[31m please switch to root user \e[0m"
 else
-    echo -e "\e[32m you are in root user"
+    echo -e "\e[32m you are in root user \e[0m"
 fi
 
 yum install mysql -y &>>$logfile
