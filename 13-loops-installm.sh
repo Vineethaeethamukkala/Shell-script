818b18ba2f3d3f757da1e28ba2ed2a54dd779585
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 23:26:26 +0530

    hellow-world2

--- 13-loops-installm.sh
+++ 13-loops-installm.sh
@@ -6,13 +6,23 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-validate(){
-    if ( $? -ne 0)
+validate() {   
+    
+    if [ $? -ne 0 ]
     then
         echo "$2 $R .. filure $N"
     else
         echo "$2 $G .. Success $N"
 }
+
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "switch to root user"
+    exit 1
+else
+    echo "you are in root user"
+
 for i in $@
 do
     yum install $i -y &>>$logfile
