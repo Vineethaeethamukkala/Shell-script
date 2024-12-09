13a94a54c63e55f6c913b418d076f7049918c104
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 22:46:03 +0530

    hellow-world2

--- 11-functions-logs.sh
+++ 11-functions-logs.sh
@@ -3,19 +3,22 @@ scriptname=$0
 date=$(date "+%F %H:%M:%S")
 logfile=/tmp/$scriptname-$date.log
 userid=$(id -u)
-validate() {
-    if [ $userid -ne 0 ]
-    then
-        echo -e "\e[31m please switch to root user"
-    else
-        echo -e "\e[32m you are in root user"
-}
 
-if [ $? -ne 0 ]
-then
-    echo -e "\e[31m $2 .. failure"
-else
+validate() {
+    if [ $? -ne 0 ]
+    then
+       echo -e "\e[31m $2 .. failure"
+    else
     echo -e "\e[32m $2 ... success"
+
+    fi
+}
+if [ $userid -ne 0 ]
+then
+    echo -e "\e[31m please switch to root user"
+else
+    echo -e "\e[32m you are in root user"
+fi
 
 yum install mysql -y &>>$logfile
 validate $? "mysql installion"
