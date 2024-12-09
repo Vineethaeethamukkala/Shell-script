cb039600f41373a6a3d33faab25aea0dadcab94d
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 22:03:53 +0530

    hellow-world2

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,8 +4,17 @@ userid=$(id -u)  #here it will check if it is root / normal user
 if [ $userid -ne 0 ]
 then
     echo "error please swith to root user"
+    exit 1
 else
     echo "your are in root user"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation mysql is failure"
+    exit 1
+else
+    echo "installation of mysql is sucess"
+fi
