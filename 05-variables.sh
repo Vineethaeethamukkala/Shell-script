0348420b0d886b43f0cfad446b7d66c60524930a
vineethaeethamukkala <vineethaeethamukkala23@gmail.com>
2024-12-09 21:31:43 +0530

    hellow-world2

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "please enter username"
 read -s username
-echo " your username id $username"
+echo " your username is $username"
+
+echo "please enter you password"
+read -s password
+echo "your password is $password"
